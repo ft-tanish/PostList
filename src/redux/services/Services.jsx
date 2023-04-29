e2057945928daa0ce6480d92610e8525af964db5
Tanish <tanishchaurasia6@gmail.com>
2023-04-29 21:37:39 +0530

    fetch-promis

--- src/redux/services/Services.jsx
+++ src/redux/services/Services.jsx
@@ -1,59 +1,103 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 // Get API
+// export const getAPI = createAsyncThunk(
+//     'api/all',
+//     async (rejectWithValue) => {
+//         try {
+//             const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+//             const data = await response?.json();
+//             return data
+//         } catch (error) {
+//             return rejectWithValue("error")
+//         }
+//     }
+// )
 export const getAPI = createAsyncThunk(
     'api/all',
     async (rejectWithValue) => {
-        try {
-            const response = await fetch("https://jsonplaceholder.typicode.com/posts")
-            const data = await response?.json();
-            return data
-        } catch (error) {
-            return rejectWithValue("error")
-        }
+        return fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(response => response?.json())
+            .catch(error => rejectWithValue("error"));
     }
-)
+);
 
 // SinglePost Data
+// export const getSinglePost = createAsyncThunk(
+//     'api/single',
+//     async (id, rejectWithValue) => {
+//         try {
+//             const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+//             const data = await response?.json();
+//             return data
+//         } catch (error) {
+//             return rejectWithValue("error")
+//         }
+//     }
+// )
+
 export const getSinglePost = createAsyncThunk(
     'api/single',
     async (id, rejectWithValue) => {
-        try {
-            const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-            const data = await response?.json();
-            return data
-        } catch (error) {
-            return rejectWithValue("error")
-        }
+        return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            .then(response => response.json())
+            .catch(error => rejectWithValue("error"));
     }
-)
+);
 
 getSinglePost.cancel = () => ({ type: 'api/single/rejected', meta: { requestId: getSinglePost.typePrefix } });
 
 // Add Post
-// export const addPost = (newPost) => ({ type: 'api/addPost', payload: newPost });
+// export const addPost = createAsyncThunk(
+//     "api/add",
+//     async (newPost, { rejectWithValue }) => {
+//         try {
+//             const response = await fetch(
+//                 "https://jsonplaceholder.typicode.com/posts",
+//                 {
+//                     method: "POST",
+//                     body: JSON.stringify({
+//                         title: newPost,
+//                         body: "",
+//                         userId: 1,
+//                     }),
+//                     headers: {
+//                         "Content-type": "application/json; charset=UTF-8",
+//                     },
+//                 }
+//             );
+//             const data = await response.json();
+//             return data;
+//         } catch (error) {
+//             return rejectWithValue("error");
+//         }
+//     }
+// );
+
 export const addPost = createAsyncThunk(
     "api/add",
     async (newPost, { rejectWithValue }) => {
-        try {
-            const response = await fetch(
-                "https://jsonplaceholder.typicode.com/posts",
-                {
-                    method: "POST",
-                    body: JSON.stringify({
-                        title: newPost,
-                        body: "",
-                        userId: 1,
-                    }),
-                    headers: {
-                        "Content-type": "application/json; charset=UTF-8",
-                    },
-                }
-            );
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            return rejectWithValue("error");
-        }
+        return fetch(
+            "https://jsonplaceholder.typicode.com/posts",
+            {
+                method: "POST",
+                body: JSON.stringify({
+                    title: newPost,
+                    body: "",
+                    userId: 1,
+                }),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8",
+                },
+            }
+        )
+            .then(response => response.json())
+            .catch(error => rejectWithValue("error"));
     }
 );
+
+
+
+
+
+
